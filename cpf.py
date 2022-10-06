d95dca52cb3924469c949f4c41bcc99f05575c93
CAMILA ZELENOVSKY <8160513@CLARETIANO.REDE>
2022-10-06 20:27:06 -0300

    adicionando comentários

--- cpf.py
+++ cpf.py
@@ -2,9 +2,11 @@ cpf = input("Digite seu cpf: ")
 soma = 0
 
 # try:
+# Achando a soma dos valores do cpf
 for x in cpf:
     soma = soma + int(x)
     print(soma)
+# Verificando se o cpf é válido
 if str(soma)[0] == str(soma)[1]:
     print("Seu cpf é válido")
 else:
