38f8369b7652ea7adcab231add23c20bc830c828
Camila Zelenovsky <67036072+czele@users.noreply.github.com>
2022-10-27 19:44:13 -0300

    Update cpf.py

--- cpf.py
+++ cpf.py
@@ -1,17 +1,18 @@
-cpf = input("Digite seu cpf: ")
+from curses.ascii import isdigit
+
+# Caso o programa dê erro instale a lib: pip install windows-curses
+cpf = input("Digite seu cpf (apenas números): ")
 soma = 0
 
-# try:
+if cpf.isdigit() == True:
 # Achando a soma dos valores do cpf
-for x in cpf:
-    soma = soma + int(x)
+    for x in cpf:
+        soma = soma + int(x)
     print(soma)
 # Verificando se o cpf é válido
-if str(soma)[0] == str(soma)[1]:
-    print("Seu cpf é válido")
+    if str(soma)[0] == str(soma)[1]:
+        print("Seu cpf é válido")
+    else:
+        print("Seu cpf não é válido")
 else:
-    print("Seu cpf não é válido")
-  
-# else:
-#     print("Digite seu cpf sem pontos e números")
-
+    print("Por favor, digite apenas números")
