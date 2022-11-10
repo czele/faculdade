78a86406c4db4fbc56c965e0d44b82f2837c6edf
Camila Zelenovsky <camila_zele@hotmail.com>
2022-11-10 20:09:39 -0300

    função calcMedia

---
+++ media-array.py
@@ -0,0 +1,8 @@
+nota1=float(input("Qual a nota da 1ª prova (caso deja parar, digite SAIR): "))
+nota2=float(input("Qual a nota da 2ª prova (caso deja parar, digite SAIR): "))
+
+def calcMedia(nota1, nota2):
+    media=(nota1 + nota2)/2
+    print(media)
+
+calcMedia()
