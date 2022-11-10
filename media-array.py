b11519b3a4204a198c91a4c4ca9569564519192e
Camila Zelenovsky <camila_zele@hotmail.com>
2022-11-10 20:41:19 -0300

    Não finalizado

--- media-array.py
+++ media-array.py
@@ -1,8 +1,19 @@
-nota1=float(input("Qual a nota da 1ª prova (caso deja parar, digite SAIR): "))
-nota2=float(input("Qual a nota da 2ª prova (caso deja parar, digite SAIR): "))
+alunosclasse=int(input("Qual o número de alunos que a classe possuí: "))
 
-def calcMedia(nota1, nota2):
+for x in range(alunosclasse):
+    nota1=int(input("Qual a nota da 1ª prova do ", , ": "))
+    nota2=int(input("Qual a nota da 2ª provan do", , ": "))
+
+def calcMedia():
     media=(nota1 + nota2)/2
     print(media)
 
 calcMedia()
+
+
+# nota1=int(input("Qual a nota da 1ª prova (caso deja parar: "))
+# nota2=int(input("Qual a nota da 2ª prova (caso deja parar: "))
+
+# Média de cada aluno
+# Média da turma
+# Maior média
